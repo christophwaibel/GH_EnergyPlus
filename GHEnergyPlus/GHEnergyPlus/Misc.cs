a7210e6cfbae38dd3c05b2b2b07c8cc79f27f359
Christoph Waibel <christophwaibel@gmx.de>
2017-11-18 17:38:41 +0100

    Add files via upload

---
+++ GHEnergyPlus/GHEnergyPlus/Misc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GHEnergyPlus
+{
+    internal static class Misc
+    {
+        internal static void RunEplus(string FileName, string command)
+        {
+            string eplusexe = FileName;
+            System.Diagnostics.Process P = new System.Diagnostics.Process();
+            P.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            P.StartInfo.FileName = eplusexe;
+            P.StartInfo.Arguments = command;
+            P.Start();
+            P.WaitForExit();
+        }
+
+    }
+}
